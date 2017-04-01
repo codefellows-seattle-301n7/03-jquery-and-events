31c21d52fc4c7c00cb806036f701a0dd219b0dd1
0smium <steve.walsh@transfueler.com>
2017-04-01 14:24:34 -0700

    fixed TODO 2 in article.js line 18

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,6 +40,18 @@ articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + this.val + '"]').fadeIn('slow');
+      debugger;
+    } else {
+    /* Otherwise, we should:
+        1. Show all the articles except the template */
+        $("article").not(".template").fadeIn('slow');
+    }
+    $('#category-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function () {
@@ -67,3 +79,4 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
